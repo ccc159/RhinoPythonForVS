518ac7c8fcd87542d6d9193e39d29843214a2941
Jingcheng Chen <chen@d2p.ch>
2019-03-08 12:40:58 +0100

    added import manager form and initialized model

---
+++ RhinoPythonForVisualStudio/ImportManager.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace RhinoPythonForVisualStudio
+{
+    partial class ImportManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxPath = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Module Search Paths";
+            // 
+            // listBoxPath
+            // 
+            this.listBoxPath.FormattingEnabled = true;
+            this.listBoxPath.HorizontalScrollbar = true;
+            this.listBoxPath.Items.AddRange(new object[] {
+            "C:\\Program Files\\Rhinoceros 5 (64-bit)\\Plug-ins\\IronPython\\Lib",
+            "C:\\Users\\jch\\AppData\\Roaming\\McNeel\\Rhinoceros\\5.0\\Plug-ins\\IronPython (814d908a-" +
+                "e25c-493d-97e9-ee3861957f49)\\settings\\lib",
+            "C:\\Users\\jch\\AppData\\Roaming\\McNeel\\Rhinoceros\\5.0\\scripts",
+            "C:\\Users\\jch\\Documents\\Projects\\D2P_Library"});
+            this.listBoxPath.Location = new System.Drawing.Point(15, 34);
+            this.listBoxPath.Name = "listBoxPath";
+            this.listBoxPath.Size = new System.Drawing.Size(663, 212);
+            this.listBoxPath.TabIndex = 1;
+            // 
+            // ImportManager
+            // 
+            this.ClientSize = new System.Drawing.Size(695, 329);
+            this.Controls.Add(this.listBoxPath);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ImportManager";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.ImportManager_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxPath;
+    }
+}
---
+++ RhinoPythonForVisualStudio/ImportManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RhinoPythonForVisualStudio
+{
+    public partial class ImportManager : Form
+    {
+        public ImportManagerModel Model;
+        public ImportManager()
+        {
+            InitializeComponent();
+            Model = new ImportManagerModel();
+            
+        }
+
+        private void ImportManager_Load(object sender, EventArgs e)
+        {
+            listBoxPath.Items.Clear();
+            foreach (var path in Model.Paths)
+            {
+                listBoxPath.Items.Add(path);
+            }
+        }
+    }
+
+    /// <summary>
+    /// This is the import manager model that deal with import data
+    /// </summary>
+    public class ImportManagerModel
+    {
+        private static string ImportFilePath = @"import.targets";
+        private static string TemplateDoc = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+
+                                            "<Project DefaultTargets = \"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\" ToolsVersion=\"4.0\">\n"+
+                                            "<PropertyGroup>\n"+
+                                            "<SearchPath>\n"+
+                                            "Hello World\n"+
+                                            "</SearchPath>\n"+
+                                            "</PropertyGroup>\n"+
+                                            "</Project>";
+
+        public string[] Paths;
+        public static string[] LoadImportFile()
+        {
+            // read import.targets xml file
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(ImportFilePath);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                doc.LoadXml(TemplateDoc);
+            }
+
+            // read search path nodes
+            XmlNode node = doc.DocumentElement?.GetElementsByTagName("SearchPath")[0];
+            // read its inner text
+            string[] paths = node?.InnerText.Split(';');
+
+            return paths;
+        }
+
+        public ImportManagerModel()
+        {
+            Paths = LoadImportFile();
+        }
+    }
+}
