f49809c42dfc4ac9f34f60e8aa641ce37ae89584
Jingcheng Chen <chen@d2p.ch>
2019-03-12 09:17:37 +0100

    added importmanager UI

--- RhinoPythonForVisualStudio/ImportManager.Designer.cs
+++ RhinoPythonForVisualStudio/ImportManager.Designer.cs
@@ -28,8 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label2 = new System.Windows.Forms.Label();
             this.listBoxPath = new System.Windows.Forms.ListBox();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonDefault = new System.Windows.Forms.Button();
+            this.toolTipModule = new System.Windows.Forms.ToolTip(this.components);
+            this.FortoopTip = new System.Windows.Forms.Label();
+            this.listBoxDll = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.forDLLTooltip = new System.Windows.Forms.Label();
+            this.toolTipDll = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonReloadProject = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
@@ -53,17 +66,140 @@
             "C:\\Users\\jch\\Documents\\Projects\\D2P_Library"});
             this.listBoxPath.Location = new System.Drawing.Point(15, 34);
             this.listBoxPath.Name = "listBoxPath";
-            this.listBoxPath.Size = new System.Drawing.Size(663, 212);
+            this.listBoxPath.Size = new System.Drawing.Size(472, 212);
             this.listBoxPath.TabIndex = 1;
+            this.listBoxPath.DoubleClick += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPath.Location = new System.Drawing.Point(15, 34);
+            this.textBoxPath.Multiline = true;
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(472, 212);
+            this.textBoxPath.TabIndex = 2;
+            this.textBoxPath.Visible = false;
+            this.textBoxPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPath_KeyDown);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Location = new System.Drawing.Point(376, 262);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(54, 28);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(319, 262);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(54, 28);
+            this.buttonEdit.TabIndex = 4;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(433, 262);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(54, 28);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonDefault
+            // 
+            this.buttonDefault.Location = new System.Drawing.Point(16, 262);
+            this.buttonDefault.Name = "buttonDefault";
+            this.buttonDefault.Size = new System.Drawing.Size(94, 28);
+            this.buttonDefault.TabIndex = 6;
+            this.buttonDefault.Text = "Restore Defaults";
+            this.buttonDefault.UseVisualStyleBackColor = true;
+            this.buttonDefault.Click += new System.EventHandler(this.buttonDefault_Click);
+            // 
+            // toolTipModule
+            // 
+            this.toolTipModule.AutoPopDelay = 20000;
+            this.toolTipModule.InitialDelay = 100;
+            this.toolTipModule.ReshowDelay = 100;
+            // 
+            // FortoopTip
+            // 
+            this.FortoopTip.AutoSize = true;
+            this.FortoopTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FortoopTip.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.FortoopTip.Location = new System.Drawing.Point(116, 9);
+            this.FortoopTip.Name = "FortoopTip";
+            this.FortoopTip.Size = new System.Drawing.Size(13, 13);
+            this.FortoopTip.TabIndex = 7;
+            this.FortoopTip.Text = "?";
+            this.FortoopTip.MouseHover += new System.EventHandler(this.FortoopTip_MouseHover);
+            // 
+            // listBoxDll
+            // 
+            this.listBoxDll.BackColor = System.Drawing.Color.Gainsboro;
+            this.listBoxDll.FormattingEnabled = true;
+            this.listBoxDll.HorizontalScrollbar = true;
+            this.listBoxDll.Location = new System.Drawing.Point(496, 34);
+            this.listBoxDll.Name = "listBoxDll";
+            this.listBoxDll.Size = new System.Drawing.Size(171, 212);
+            this.listBoxDll.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(493, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Referenced DLL";
+            // 
+            // forDLLTooltip
+            // 
+            this.forDLLTooltip.AutoSize = true;
+            this.forDLLTooltip.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forDLLTooltip.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.forDLLTooltip.Location = new System.Drawing.Point(574, 9);
+            this.forDLLTooltip.Name = "forDLLTooltip";
+            this.forDLLTooltip.Size = new System.Drawing.Size(13, 13);
+            this.forDLLTooltip.TabIndex = 10;
+            this.forDLLTooltip.Text = "?";
+            this.forDLLTooltip.MouseHover += new System.EventHandler(this.forDLLTooltip_MouseHover);
+            // 
+            // buttonReloadProject
+            // 
+            this.buttonReloadProject.Location = new System.Drawing.Point(116, 262);
+            this.buttonReloadProject.Name = "buttonReloadProject";
+            this.buttonReloadProject.Size = new System.Drawing.Size(94, 28);
+            this.buttonReloadProject.TabIndex = 11;
+            this.buttonReloadProject.Text = "Reload Project";
+            this.buttonReloadProject.UseVisualStyleBackColor = true;
+            this.buttonReloadProject.Click += new System.EventHandler(this.buttonReloadProject_Click);
             // 
             // ImportManager
             // 
-            this.ClientSize = new System.Drawing.Size(695, 329);
+            this.ClientSize = new System.Drawing.Size(682, 309);
+            this.Controls.Add(this.buttonReloadProject);
+            this.Controls.Add(this.forDLLTooltip);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxDll);
+            this.Controls.Add(this.FortoopTip);
+            this.Controls.Add(this.buttonDefault);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxPath);
             this.Controls.Add(this.listBoxPath);
             this.Controls.Add(this.label2);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "ImportManager";
             this.ShowIcon = false;
+            this.Text = "Import Manager";
             this.Load += new System.EventHandler(this.ImportManager_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -75,5 +211,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox listBoxPath;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonDefault;
+        private System.Windows.Forms.ToolTip toolTipModule;
+        private System.Windows.Forms.Label FortoopTip;
+        private System.Windows.Forms.ListBox listBoxDll;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label forDLLTooltip;
+        private System.Windows.Forms.ToolTip toolTipDll;
+        private System.Windows.Forms.Button buttonReloadProject;
     }
 }
--- RhinoPythonForVisualStudio/ImportManager.cs
+++ RhinoPythonForVisualStudio/ImportManager.cs
@@ -6,29 +6,139 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using EnvDTE;
+using EnvDTE80;
+using String = System.String;
 
 namespace RhinoPythonForVisualStudio
 {
     public partial class ImportManager : Form
     {
-        public ImportManagerModel Model;
-        public ImportManager()
+        public ImportManagerModel ImportModel;
+        public DLLManagerModel DllModel;
+        public _DTE dte;
+        
+        public ImportManager(string ProjectPath,_DTE _dte)
         {
             InitializeComponent();
-            Model = new ImportManagerModel();
-            
+            ImportModel = new ImportManagerModel(ProjectPath);
+            DllModel = new DLLManagerModel(ProjectPath);
+            dte = _dte;
         }
 
         private void ImportManager_Load(object sender, EventArgs e)
         {
+            updateListBoxContent();
+            updateDllBoxContent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // switch from text box to list box
+            listBoxPath.Visible = true;
+            textBoxPath.Visible = false;
+            // get raw path
+            var rawText = textBoxPath.Text;
+            // read its inner text
+            string[] paths = rawText.Replace("\n","").Replace("\r", "").Split(';');
+            // sanitize paths
+            paths = paths.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            // set path to model
+            ImportModel.Paths = paths;
+            // update listbox
+            updateListBoxContent();
+            // save the file
+            ImportModel.SaveImportFile();
+            // disable itself
+            buttonSave.Enabled = false;
+        }
+
+        private void updateListBoxContent()
+        {
             listBoxPath.Items.Clear();
-            foreach (var path in Model.Paths)
+            foreach (var path in ImportModel.Paths) listBoxPath.Items.Add(path);
+        }
+
+        private void updateDllBoxContent()
+        {
+            listBoxDll.Items.Clear();
+            foreach (var path in DllModel.Paths) listBoxDll.Items.Add(path);
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            // switch from listbox to text box
+            listBoxPath.Visible = false;
+            textBoxPath.Visible = true;
+            // load paths into textbox
+            textBoxPath.Clear();
+            textBoxPath.Text = String.Join(";\r\n", ImportModel.Paths) + ";";
+            textBoxPath.Focus();
+            // enable save button
+            buttonSave.Enabled = true;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            // close the import manager
+            Close();
+            Dispose();
+        }
+
+        private void textBoxPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
             {
-                listBoxPath.Items.Add(path);
+                // switch from text box to list box
+                listBoxPath.Visible = true;
+                textBoxPath.Visible = false;
             }
+        }
+
+        private void buttonDefault_Click(object sender, EventArgs e)
+        {
+            ImportModel.ResetToDefaultPaths();
+            // switch from text box to list box
+            listBoxPath.Visible = true;
+            textBoxPath.Visible = false;
+            // update list box
+            updateListBoxContent();
+            // save the file
+            ImportModel.SaveImportFile();
+        }
+
+        private void FortoopTip_MouseHover(object sender, EventArgs e)
+        {
+            string tooltip = "This contains user-specific import search paths for IronPython.\n\n";
+            tooltip += "For a project, usually:\n";
+            tooltip += "* your own python libraries folder.\n";
+            tooltip += "* Rhino System folder (for RhinoCommon).\n";
+            tooltip += "* rhinoscriptsyntax package (have a look in Rhino's Python Editor (Tools -> Options) to find it.\n";
+            tooltip += "* Path of .NET Reference Assemblies.\n\n";
+            tooltip += "Be aware that every path has to end with a ';'\n";
+            tooltip += "THIS FILE IS PER-USER! Please make sure '*.targets' is in your git ignore list.";
+            toolTipModule.SetToolTip(FortoopTip, tooltip);
+        }
+
+        private void forDLLTooltip_MouseHover(object sender, EventArgs e)
+        {
+            string tooltip = "Please edit DLL references directly in '__init__.py' file\n";
+            tooltip += "under your project root.";
+            toolTipModule.SetToolTip(forDLLTooltip, tooltip);
+        }
+
+        private void buttonReloadProject_Click(object sender, EventArgs e)
+        {
+            // reload project
+
+            string solutionPath = dte.Solution.FullName;
+
+            dte.ExecuteCommand("File.CloseSolution");
+            dte.Solution.Open(solutionPath);
         }
     }
 
@@ -37,41 +147,130 @@ namespace RhinoPythonForVisualStudio
     /// </summary>
     public class ImportManagerModel
     {
-        private static string ImportFilePath = @"import.targets";
-        private static string TemplateDoc = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+
+        private static readonly string FileName = @"import.targets";
+        private static readonly string TemplateDoc = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"+
                                             "<Project DefaultTargets = \"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\" ToolsVersion=\"4.0\">\n"+
                                             "<PropertyGroup>\n"+
                                             "<SearchPath>\n"+
-                                            "Hello World\n"+
+                                            "C:\\Program Files\\Rhinoceros 5 (64-bit)\\Plug-ins\\IronPython\\Lib;\n"+
                                             "</SearchPath>\n"+
                                             "</PropertyGroup>\n"+
                                             "</Project>";
 
+        private static readonly string[] defaultPaths = {"C:\\Program Files\\Rhinoceros 5 (64-bit)\\Plug-ins\\IronPython\\Lib"};
+        public string ImportFilePath;
         public string[] Paths;
-        public static string[] LoadImportFile()
+        public XmlDocument Doc;
+        /// <summary>
+        /// Load import file from target.import
+        /// </summary>
+        /// <returns>a list from path read from target.import</returns>
+        public string[] LoadImportFile()
         {
             // read import.targets xml file
-            XmlDocument doc = new XmlDocument();
             try
             {
-                doc.Load(ImportFilePath);
+                Doc.Load(ImportFilePath);
             }
             catch (System.IO.FileNotFoundException)
             {
-                doc.LoadXml(TemplateDoc);
+                Doc.LoadXml(TemplateDoc);
+                Doc.Save(ImportFilePath);
             }
 
             // read search path nodes
-            XmlNode node = doc.DocumentElement?.GetElementsByTagName("SearchPath")[0];
+            XmlNode node = Doc.DocumentElement?.GetElementsByTagName("SearchPath")[0];
             // read its inner text
-            string[] paths = node?.InnerText.Split(';');
+            string[] paths = node?.InnerText.Replace("\n", "").Replace("\r", "").Split(';');
+             
+            // sanitize path
+            paths = paths?.Where(x => !string.IsNullOrEmpty(x)).ToArray();
 
             return paths;
         }
 
-        public ImportManagerModel()
+        /// <summary>
+        /// Save the file into target.import
+        /// </summary>
+        public void SaveImportFile()
         {
-            Paths = LoadImportFile();
+            // put new paths into doc
+            XmlNode node = Doc.DocumentElement?.GetElementsByTagName("SearchPath")[0];
+            node.InnerText = String.Join(";\n",Paths) + ";";
+            // save
+            Doc.Save(ImportFilePath);
         }
+
+        /// <summary>
+        /// Reset model path to default path
+        /// </summary>
+        public void ResetToDefaultPaths()
+        {
+            Paths = defaultPaths;
+        }
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ImportManagerModel(string projectPath)
+        {
+
+            ImportFilePath = Path.Combine(projectPath, FileName);
+            Doc = new XmlDocument();
+            Paths = LoadImportFile();
+            
+        }
+    }
+
+    public class DLLManagerModel
+    {
+        private static readonly string FileName = @"__init__.py";
+
+        private static readonly string templateDoc = "\"\"\"\n" +
+                                                     "This file contains references to external DLLs.\n" +
+                                                     "Feel free to add as required.\n" +
+                                                     "Please do not put any paths in here, use RhinoPython -> ImportManager instead!\n" +
+                                                     "\"\"\"\n\n" +
+                                                     "import clr\n" +
+                                                     "clr.AddReference(\"RhinoCommon.dll\")";
+
+        public string Doc;
+        public string[] Paths;
+
+        /// <summary>
+        /// Load import file from target.import
+        /// </summary>
+        /// <returns>a list from path read from target.import</returns>
+        public DLLManagerModel(string projectPath)
+        {
+            var filePath = Path.Combine(projectPath, FileName);
+            try
+            {
+                // read __init__ file
+                Doc = File.ReadAllText(filePath, Encoding.UTF8);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                // create a init file
+                Doc = templateDoc;
+                File.WriteAllText(filePath, Doc);
+            }
+            
+
+            // parse the file into paths
+            var matchedGroups = Regex.Matches(Doc, @"AddReference\(([^)]*)\)");
+            
+            string[] paths = new string[matchedGroups.Count];
+
+            for (int i = 0; i < matchedGroups.Count; i++)
+            {
+                var value = matchedGroups[i].Value;
+                paths[i] = value.Substring(14, value.Length - 16);
+            }
+
+            Paths = paths;
+        }
+
     }
 }
